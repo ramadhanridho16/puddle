b56f3042b6c33849b6b1190e58cf0cbe3cb585f3
Ramadhan Ridho <ramadhan.ridho.16@students.unnes.ac.id>
2023-06-10 17:12:06 +0700

    add item detail from database

--- puddle/urls.py
+++ puddle/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
