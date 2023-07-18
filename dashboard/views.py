d426f040812718c4319fef2aeac06ba137d969f1
Ramadhan Ridho <ramadhan.ridho.16@students.unnes.ac.id>
2023-07-18 18:24:36 +0700

    Adding searching feature

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from item.models import Item
+from item.models import Category, Item
 
+
+# @login_required
+# def index(request):
+#     items = Item.objects.filter(created_by=request.user)
+
+#     return render(request, 'dashboard/index.html', {
+#         'items': items,
+#     })
 
 @login_required
 def index(request):
-    items = Item.objects.filter(created_by=request.user)
-
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
     return render(request, 'dashboard/index.html', {
+        'categories': categories,
         'items': items,
     })
